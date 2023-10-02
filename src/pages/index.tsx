b2b46ed7c87b95ddf376d65015a9c7704dcbeeaf
Bruce <recall4056@gmail.com>
2023-10-03 02:55:22 +0800

    fix: :bug: remove svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import reactLogo from '@/assets/svg/react.svg'
-import viteLogo from '@/assets/svg/vite.svg'
 import { Button, Switch } from 'antd'
 import SunIcon from '~icons/line-md/sun-rising-loop'
 import MoonIcon from '~icons/line-md/moon-filled-alt-loop'
@@ -8,34 +6,6 @@ export function Component(): React.JSX.Element {
   const { theme, toggleTheme } = useThemeStore()
   return (
     <div className="absolute inset-0 m-auto flex h-fit w-fit flex-col space-y-4">
-      <div className="flex items-center justify-center space-x-4">
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-          rel="noreferrer"
-          className="hover:animate-bounce"
-        >
-          <img
-            src={viteLogo}
-            width="64"
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          rel="noreferrer"
-          className="hover:animate-spin"
-        >
-          <img
-            src={reactLogo}
-            width="64"
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
       <span className="text-2xl">React TypeScript Starter Template</span>
       <Button className="self-center">Get Started</Button>
 
