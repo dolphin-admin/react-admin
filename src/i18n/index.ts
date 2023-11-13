66a049b9473a5ac5cc1af05b23526717ca67aae0
Bruce <recall4056@gmail.com>
2023-11-13 10:27:15 +0800

    chore: :hammer: update

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,32 @@ import { Lang } from '@dolphin-admin/utils'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import { EN_US, ZH_CN } from '@/locales'
+/**
+ * 动态加载 i18n 资源文件
+ */
+const dynamicLoadTrans = async () => {
+  const result: Record<string, any> = {}
+  const translations = Object.entries(import.meta.glob('../locales/**/*.json')).map(
+    async ([path, translation]) => [
+      path.split('/')[2].replace('_', '-'),
+      path.match(/([^/]+)\.json$/)![1].toUpperCase(),
+      await translation()
+    ]
+  ) as Promise<[string, string, () => Promise<{ default: Record<string, string> }>]>[]
 
-const resources = {
-  [Lang['en-US']]: EN_US,
-  [Lang['zh-CN']]: ZH_CN
+  const resolvedTranslations = await Promise.all(translations)
+
+  resolvedTranslations.forEach(([lang, key, translationData]) => {
+    if (!result[lang]) {
+      result[lang] = {}
+    }
+    result[lang][key] = translationData
+  })
+
+  return result
 }
+
+export const resources = await dynamicLoadTrans()
 
 const ns = Object.keys(resources[Lang['en-US']])
 
