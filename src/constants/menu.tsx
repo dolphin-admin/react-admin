d80fba3a8a4c89f36df0cbeeb1ec319454fd8b60
Bruce <recall4056@gmail.com>
2023-11-30 00:37:39 +0800

    feat: :sparkles: error pages and breadcrumb

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -1,23 +1,23 @@
 import type { MenuItem } from '@/types'
 // import UniversalComponentsIcon from '~icons/ic/baseline-auto-awesome-mosaic'
 // import ChartsIcon from '~icons/ic/baseline-bar-chart'
-// import UnauthorizedIcon from '~icons/ic/baseline-do-not-disturb'
-// import ErrorPagesIcon from '~icons/ic/baseline-error-outline'
+import UnauthorizedIcon from '~icons/ic/baseline-do-not-disturb'
+import ErrorPagesIcon from '~icons/ic/baseline-error-outline'
 import ListTemplatesIcon from '~icons/ic/outline-list-alt'
-// import NotFoundIcon from '~icons/tabler/error-404'
+import MultiLevelMenusIcon from '~icons/ic/round-format-list-bulleted'
 import CardTemplatesIcon from '~icons/ic/round-space-dashboard'
-// import IAmATeapotIcon from '~icons/icon-park-outline/tea-drink'
-// import InternalServerErrorIcon from '~icons/lucide/server-off'
+import IAmATeapotIcon from '~icons/icon-park-outline/tea-drink'
+import InternalServerErrorIcon from '~icons/lucide/server-off'
 // import UserManagementIcon from '~icons/mdi/account-cog-outline'
 // import NavigationIcon from '~icons/mdi/compass-outline'
 // import SystemFunctionsIcon from '~icons/mdi/function-variant'
 // import DictionaryManagementIcon from '~icons/ri/booklet-line'
 import CodeTemplatesIcon from '~icons/solar/code-bold'
+import NotFoundIcon from '~icons/tabler/error-404'
 
-// TODO: Fix i18n bug
 const t = i18n.getFixedT(null, 'MENU')
 
-export const menu: MenuItem[] = [
+export const getMenuTree = (): MenuItem[] => [
   // getItem(t('Menu.Navigation'), 'Navigation', <AIcon component={NavigationIcon} />),
   // getItem(t('Menu.SystemFunctions'), 'SystemFunctions', <AIcon component={SystemFunctionsIcon} />, [
   //   getItem(t('Menu.UserManagement'), 'UserManagement', <AIcon component={UserManagementIcon} />),
@@ -29,18 +29,74 @@ export const menu: MenuItem[] = [
   // ]),
   {
     label: t('CODE.TEMPLATES'),
-    key: 'code-templates',
+    key: '/code-templates',
     icon: <AIcon component={CodeTemplatesIcon} />,
     children: [
       {
         label: t('CODE.TEMPLATES.TABLE'),
-        key: 'code-templates/table',
+        key: '/code-templates/table',
         icon: <AIcon component={ListTemplatesIcon} />
       },
       {
         label: t('CODE.TEMPLATES.CARD'),
-        key: 'code-templates/card',
+        key: '/code-templates/card',
         icon: <AIcon component={CardTemplatesIcon} />
+      }
+    ]
+  },
+  {
+    label: t('MULTI.LEVEL.MENUS'),
+    key: '/multi-level-menus',
+    icon: <AIcon component={MultiLevelMenusIcon} />,
+    children: [
+      {
+        label: '2-1',
+        key: '/multi-level-menus/2-1',
+        icon: <AIcon component={MultiLevelMenusIcon} />,
+        children: [
+          {
+            label: '2-1-1',
+            key: '/multi-level-menus/2-1/2-1-1',
+            icon: <AIcon component={MultiLevelMenusIcon} />
+          },
+          {
+            label: '2-1-2',
+            key: '/multi-level-menus/2-1/2-1-2',
+            icon: <AIcon component={MultiLevelMenusIcon} />
+          }
+        ]
+      },
+      {
+        label: '2-2',
+        key: '/multi-level-menus/2-2',
+        icon: <AIcon component={MultiLevelMenusIcon} />
+      }
+    ]
+  },
+  {
+    label: t('ERROR.PAGES'),
+    key: '/error-pages',
+    icon: <AIcon component={ErrorPagesIcon} />,
+    children: [
+      {
+        label: '403',
+        key: '/error-pages/403',
+        icon: <AIcon component={UnauthorizedIcon} />
+      },
+      {
+        label: '404',
+        key: '/error-pages/404',
+        icon: <AIcon component={NotFoundIcon} />
+      },
+      {
+        label: '418',
+        key: '/error-pages/418',
+        icon: <AIcon component={IAmATeapotIcon} />
+      },
+      {
+        label: '500',
+        key: '/error-pages/500',
+        icon: <AIcon component={InternalServerErrorIcon} />
       }
     ]
   }
@@ -50,10 +106,45 @@ export const menu: MenuItem[] = [
   //   <AIcon component={UniversalComponentsIcon} />,
   //   [getItem(t('Menu.Charts'), 'Charts', <AIcon component={ChartsIcon} />)]
   // ),
-  // getItem(t('Menu.ErrorPages'), 'ErrorPages', <AIcon component={ErrorPagesIcon} />, [
-  //   getItem(t('Menu.403'), 'Unauthorized', <AIcon component={UnauthorizedIcon} />),
-  //   getItem(t('Menu.404'), 'NotFound', <AIcon component={NotFoundIcon} />),
-  //   getItem(t('Menu.418'), 'IAmATeapot', <AIcon component={IAmATeapotIcon} />),
-  //   getItem(t('Menu.500'), 'InternalServerError', <AIcon component={InternalServerErrorIcon} />)
-  // ])
 ]
+
+// 菜单数据缓存
+export const menuCacheMap = new Map<string, MenuItem | undefined>()
+
+/**
+ * 根据当前路由路径递归匹配菜单数据
+ * @description 先从缓存中获取，如果缓存中没有，则递归匹配，匹配到后缓存结果
+ */
+export function getMenuItem(key: string, menuTree: MenuItem[]): MenuItem | undefined {
+  // 优先从缓存中获取
+  if (menuCacheMap.has(key)) {
+    return menuCacheMap.get(key)
+  }
+
+  // 匹配当前菜单数据
+  const menu = menuTree.find((m) => m?.key === key)
+  if (menu) {
+    // 缓存结果
+    menuCacheMap.set(key, menu)
+    return menu
+  }
+
+  // 递归匹配子路由
+  // eslint-disable-next-line no-restricted-syntax
+  for (const r of menuTree) {
+    const { children } = (r as any) ?? {}
+    if (children) {
+      const menuItem = getMenuItem(key, children)
+      if (menuItem) {
+        // 缓存结果
+        menuCacheMap.set(key, menuItem)
+        return menuItem
+      }
+    } else {
+      return undefined
+    }
+  }
+  // 缓存结果
+  menuCacheMap.set(key, undefined)
+  return undefined
+}
